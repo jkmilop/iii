7d8fb1dc48d6e97a6a91e88177734f3d6a7e8a04
Julio Mantilla <=>
2023-04-19 14:43:55 -0500

    Se modifica Back para integración

--- Back/app.js
+++ Back/app.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const appRoutes =  require('./app/routes/routes')
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express()
 
 const port = process.env.PORT || 3000;
 
+app.set("port", port);
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", appRoutes);
-app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 
 app.listen(port, () => console.log(`listening at port ${port}`))
--- Back/app/config/db.js
+++ Back/app/config/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user : "postgres",
-    host : "localhost",
-    database : "",
-    password : "",
-    port : 5432,
+    user : process.env.DB_USER || "",
+    host : process.env.DB_HOST || "localhost",
+    database : process.env.DB_DATABASE || "",
+    password : process.env.DB_PASSWORD || "",
+    port : process.env.DB_PORT || 5432,
 })
 
 /* pool.connect();
