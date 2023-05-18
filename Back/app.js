b8cef874b93ac4b54e4f7fef3cb9b97f26847bfb
Julio Cesar Mantilla Ramirez <jmantilla23988@gmail.com>
2023-05-18 16:31:22 -0500

    Se modifica los valores de la bd

--- Back/app.js
+++ Back/app.js
@@ -3,7 +3,7 @@ const appRoutes =  require('./app/routes/routes')
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express()
- 
+
 const port = process.env.PORT || 3000;
 
 app.set("port", port);
